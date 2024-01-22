e9f28e56e1a6553aec1fbffd8d8a9d116697f62e
mahmud5658 <almahmuda5@gmail.com>
2024-01-22 23:39:48 +0600

    python basic

---
+++ introduction/format.py
@@ -0,0 +1,4 @@
+name = "Abdullah"
+id = 5658
+
+print(f"My name is {name} & my id is {id}")
--- introduction/main.py
+++ introduction/main.py
@@ -1,1 +1,8 @@
 print("Abdullah Al Mahmud")
+
+# abdullah al mahmud // comment 
+
+"'multiline comment  Abdulah Al Mahmud. Daffodil International University'"
+
+name = "Abdullah al Mahmud"
+print(name)
