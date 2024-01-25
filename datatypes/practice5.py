7412b3474b245cc8826b525e3bd6c225d1f53213
mahmud5658 <almahmuda5@gmail.com>
2024-01-25 14:21:51 +0600

    python basic

--- datatypes/practice5.py
+++ datatypes/practice5.py
@@ -1,2 +1,4 @@
 name = {1:"Abdullah",2:"Akash",3:"Bappy",4:"Moinul"}
 print(name)
+print(name.keys())
+print(name.values())
---
+++ datatypes/practice6.py
@@ -0,0 +1,9 @@
+set1 = {
+    'Abdullah',2,'Akash',5.0,True,'Bappy',2
+}
+
+print(type(set1))
+
+print(set1)
+
+# set ignore the duplicate values
