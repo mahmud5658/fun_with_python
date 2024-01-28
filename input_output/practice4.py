d408258863930adfd8e52ebf8052a02479f4eeb2
mahmud5658 <almahmuda5@gmail.com>
2024-01-28 14:37:16 +0600

    python practice

---
+++ input_output/practice4.py
@@ -0,0 +1,15 @@
+x,y = input("Enter two values: ").split()
+print("Number of boys: ",x)
+print("Number of girls: ",y)
+
+a,b,c = input("Enter three value: ").split()
+print("Total number of students: ",a)
+print("Number of boys: ",b)
+print("Number of girls: ",c)
+
+m,n = input("Enter two values: ").split()
+print("First number is: {} and second number is: {}".format(m,n))
+
+l1 = list(map(int,input("Enter multiple values: ").split()))
+print("List of students: ",l1)
+
