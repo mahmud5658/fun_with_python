1a05095054e18ce8a755f3593f4b02135e06c0ad
mahmud5658 <almahmuda5@gmail.com>
2024-01-28 14:50:33 +0600

    python practice

---
+++ input_output/practice5.py
@@ -0,0 +1,12 @@
+x,y = [int(i) for i in input("Enter two number: ").split()]
+
+print("First number: ",x)
+print("Second number: ",y)
+
+x,y,z = [int(i) for i in input("Enter three number: ").split()]
+
+print("First number is: {}, second number is: {} and third number is: {}".format(x,y,z))
+
+
+x = [int(i) for i in input("Enter multiple values: ").split()]
+print("Number of list is: ",x)
