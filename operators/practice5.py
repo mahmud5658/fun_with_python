6b71181cac6a802c0bf7786519d3a210f607672f
mahmud5658 <almahmuda5@gmail.com>
2024-01-30 00:06:43 +0600

    python basics

---
+++ operators/practice5.py
@@ -0,0 +1,8 @@
+a,b = [int(i) for i in input("Enter two number: ").split()]
+
+if a>b:
+    print("a is greater than b")
+elif a<b:
+    print("a is less than b")
+else:
+    print("a is equal to b")
